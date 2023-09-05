883b65de5937b2a7a5860372a772879c35f486b4
Tomas Piaggio <tomas.piaggio12@gmail.com>
2023-09-04 23:37:21 -0300

    Initial files

--- src/message_collector/__main__.py
+++ src/message_collector/__main__.py
@@ -2,25 +2,25 @@ import argparse
 import json
 from typing import Any, Dict
 
-import pychat
+import pytchat
 import clickhouse_connect
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Process ClickHouse and YouTube arguments.")
-    
+
     # ClickHouse arguments
     parser.add_argument('--clickhouse_host', required=True, help='ClickHouse host.')
     parser.add_argument('--clickhouse_port', required=True, type=int, help='ClickHouse port.')
     parser.add_argument('--clickhouse_username', required=True, help='ClickHouse username.')
     parser.add_argument('--clickhouse_password', required=True, help='ClickHouse password.')
-    
+
     # YouTube argument
     parser.add_argument('--video-id', required=True, help='YouTube video ID.')
-    
+
     # Name to differentiate streams
     parser.add_argument('--stream_name', required=True, help='The friendly name of the stream.')
-    
+
     return parser.parse_args()
 
 
@@ -35,38 +35,40 @@ if __name__ == "__main__":
     video_id = args.video_id
 
     client = clickhouse_connect.get_client(
-        host=clickhouse_host, 
-        port=clickhouse_port, 
-        username=clickhouse_username, 
+        host=clickhouse_host,
+        port=clickhouse_port,
+        username=clickhouse_username,
         password=clickhouse_password
     )
     # Create table if not exists.
     with open("create_table.sql", "r") as f:
         query = f.read()
         client.command(query)
-    
+
     chat = pytchat.create(video_id=video_id)
-    
 
     while chat.is_alive():
         for c in chat.get().sync_items():
-        print(f"{c.datetime} [{c.author.name}]- {c.message}")
+            print(f"{c.datetime} [{c.author.name}]- {c.message}")
 
-        json_data = c.json()
-        data = json.loads(json_data)
-        client.insert("messages", data, column_names=[
-            "name",
-            "type",
-            "id",
-            "message",
-            "messageEx",
-            "timestamp",
-            "datetime",
-            "elapsedTime",
-            "amountValue",
-            "amountString",
-            "currency",
-            "bgColor",
-            "author",
-        ])
+            json_data = c.json()
+            data = json.loads(json_data)
 
+            to_insert = {
+                'id': c.id,
+                'type': c.type,
+                'message': c.message,
+                'timestamp': c.timestamp,
+                'datetime': c.datetime,
+                'elapsedTime': c.elapsedTime,
+                'amountValue': c.amountValue,
+                'amountString': c.amountString,
+                'currency': c.currency,
+                'author_name': c.author.name,
+                'author_channelId': c.author.channelId,
+                'author_channelUrl': c.author.channelUrl,
+                'video_id': video_id,
+                'stream_name': stream_name
+            }
+            print(to_insert)
+            client.insert("messages", [[x for x in to_insert.values()]], column_names=[x for x in to_insert.keys()])
--- src/message_collector/create_table.sql
+++ src/message_collector/create_table.sql
@@ -1,29 +1,18 @@
 CREATE TABLE IF NOT EXISTS messages
 (
-    name            String,
-    type            String,
-    remarks         String,
-    id              String,
-    message         String,
-    messageEx       Array(String),
-    timestamp       Int64,
-    datetime        String,
-    elapsedTime     String,
-    amountValue     Float32,
-    amountString    String,
-    currency        String,
-    bgColor         Int32,
-    author          Nested
-    (
-        name            String,
-        channelId       String,
-        channelUrl      String,
-        imageUrl        String,
-        badgeUrl        String,
-        isVerified      UInt8,
-        isChatOwner     UInt8,
-        isChatSponsor   UInt8,
-        isChatModerator UInt8
-    )
+    id                String,
+    type              String,
+    message           String,
+    timestamp         Int64,
+    datetime          String,
+    elapsedTime       String,
+    amountValue       Float32,
+    amountString      String,
+    currency          String,
+    author_name       String,
+    author_channelId  String,
+    author_channelUrl String,
+    video_id          String,
+    stream_name       String
 ) ENGINE = MergeTree()
 ORDER BY (timestamp);

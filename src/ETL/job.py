9cbea29e0be400ee9551a94d05ffc337c1cd90d9
Tomas Piaggio <tomas.piaggio12@gmail.com>
2023-09-05 00:34:01 -0300

    Spark Clickhouse working

--- src/ETL/job.py
+++ src/ETL/job.py
@@ -1,24 +1,24 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-
-# Initialize a SparkSession
-spark = SparkSession.builder.appName("FilterAndSplit").getOrCreate()
-
-# Assuming 'df' is your PySpark DataFrame
-jxc_words = ["pato", "patricia", "bullrich", "jxc", "macri", "larreta", "pelado", "jorge"]
-lla_words = ["libertad", "milei", "piparo", "villarruel", "javier", "leon"]
-k_words = ["cristina", "kirchner", "fernandez", "massa", "sergio", "tomas", "kicillof", "alberto"]
-
-jxc_condition = col("message").rlike("|".join(jxc_words))
-lla_condition = col("message").rlike("|".join(lla_words))
-k_condition = col("message").rlike("|".join(k_words))
-
-jxc_df = df.filter(jxc_condition).drop("message")
-lla_df = df.filter(lla_condition).drop("message")
-k_df = df.filter(k_condition).drop("message")
-
-# Don't forget to stop the SparkSession when you're done
-spark.stop()
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import col
+#
+# # Initialize a SparkSession
+# spark = SparkSession.builder.appName("FilterAndSplit").getOrCreate()
+#
+# # Assuming 'df' is your PySpark DataFrame
+# jxc_words = ["pato", "patricia", "bullrich", "jxc", "macri", "larreta", "pelado", "jorge"]
+# lla_words = ["libertad", "milei", "piparo", "villarruel", "javier", "leon"]
+# k_words = ["cristina", "kirchner", "fernandez", "massa", "sergio", "tomas", "kicillof", "alberto"]
+#
+# jxc_condition = col("message").rlike("|".join(jxc_words))
+# lla_condition = col("message").rlike("|".join(lla_words))
+# k_condition = col("message").rlike("|".join(k_words))
+#
+# jxc_df = df.filter(jxc_condition).drop("message")
+# lla_df = df.filter(lla_condition).drop("message")
+# k_df = df.filter(k_condition).drop("message")
+#
+# # Don't forget to stop the SparkSession when you're done
+# spark.stop()
 
 
 from pyspark.sql import SparkSession
@@ -29,25 +29,25 @@ clickhouse_url = "jdbc:clickhouse://clickhouse:8123/messages"
 # Set up the Spark session
 spark = SparkSession.builder \
     .appName("ClickHouseDataRead") \
-    .config("spark.jars", "/path/to/clickhouse-jdbc-driver.jar")  # Provide the path to the ClickHouse JDBC driver JAR
-.getOrCreate()
+    .config("spark.jars", "./lib/clickhouse-jdbc-0.2.4.jar") \
+    .getOrCreate()
 
 # Define your connection properties
 connection_properties = {
-    "user": "<clickhouse_username>",
-    "password": "<clickhouse_password>",
+    "user": "clickhouse-user",
+    "password": "secret",
     "driver": "ru.yandex.clickhouse.ClickHouseDriver"
 }
 
 # Define the table you want to read from
-table_name = "<clickhouse_table>"
+table_name = "messages"
 
 # Read data from ClickHouse
-clickhouse_data = spark.read \
-    .jdbc(url=clickhouse_url, table=table_name, properties=connection_properties)
+clickhouse_data = spark.read.jdbc(
+    url=clickhouse_url,
+    table=table_name,
+    properties=connection_properties
+)
 
 # You can now perform operations on the clickhouse_data DataFrame
 clickhouse_data.show()
-
-# Stop the Spark session when you're done
-spark.stop()
